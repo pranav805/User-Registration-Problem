d718ffb09483897b8fd802e29aad45e9113950e1
pranav805 <pranav.ige96@gmail.com>
2020-02-14 12:30:51 +0530

    ADDED: Checked the validity of User's first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,3 +2,20 @@
 shopt -s extglob
 clear
 echo "WELCOME TO USER REGISTRATION SYSTEM"
+
+nameExpression="^[A-Z]{1}[a-z]{3,}$"
+
+function firstName(){
+   if [[ $1 =~ $2 ]]
+   then
+      echo "Valid"
+   else
+      echo "Invalid"
+   fi
+}
+
+read -p "Enter first name: " firstName
+firstName $firstName $nameExpression
+
+
+
