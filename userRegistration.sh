26e8b6fd1ece3489c5299625a55c91f9b4e30453
pranav805 <pranav.ige96@gmail.com>
2020-02-14 16:16:49 +0530

    ADDED: Checked the validity of User's Mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,8 +4,9 @@ clear
 echo "WELCOME TO USER REGISTRATION SYSTEM"
 
 nameExpression="^[A-Z]{1}[a-z]{2,}$"
-
 eMailExpression="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$"
+mobileExpression="^[0-9]{2}[ ]{1}[0-9]{10}$"
+
 function checkValidity(){
    if [[ $1 =~ $2 ]]
    then
@@ -23,5 +24,8 @@ checkValidity $lastName $nameExpression
 
 read -p "Enter your Email address: " eMail
 checkValidity $eMail $eMailExpression
+
+read -p "Enter your Mobile number: " mobileNumber
+checkValidity "$mobileNumber" "$mobileExpression"
 
 
